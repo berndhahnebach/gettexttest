58f22b4d998394db76f9a1fbc67ec44b307463f7
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-17 08:44:14 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://phrase.com/blog/posts/i18n-advantages-babel-python/
 # https://phrase.com/blog/posts/translate-python-gnu-gettext/
 # http://oz123.github.io/writings/2013-08-04-Localizing-with-gettext/index.html
+# https://inventwithpython.com/blog/2014/12/20/translate-your-python-3-program-with-the-gettext-module/
 """
 pybabel update -i locale/base.pot -d locale
 pybabel compile -d locale
@@ -13,5 +14,8 @@ localepath = os.path.join(os.getcwd(), "locale")
 print(localepath)
 new_lang = gettext.translation('base', localedir=localepath, languages=['de'])
 new_lang.install()
+#print(new_lang.__dir__())
+print(new_lang._info)
+print(gettext.find('base', 'locale'))
 
 print(gettext.gettext('This is a translatable string.'))
